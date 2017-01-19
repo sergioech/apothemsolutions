4de1bce3723e430a8ff2583d52cac129b538c561
amigabledave <amigabledave@gmail.com>
2017-01-19 13:17:45 -0600

    Creates the DatoCNBV Class on datastore

--- python_files/datastore.py
+++ python_files/datastore.py
@@ -2,3 +2,19 @@ from google.appengine.ext import ndb
 
 #--- datastore classes ----------
 
+class DatoCNBV(ndb.Model):
+	
+	created = ndb.DateTimeProperty(auto_now_add=True)	
+	archivo_fuente = ndb.StringProperty(required=True)
+	extraccion = ndb.IntegerProperty(required=True)
+
+	#Aparecen en: 040_11l_R0
+	cve_periodo = ndb.IntegerProperty()
+	cve_institucion = ndb.IntegerProperty()
+	cve_TEC = ndb.IntegerProperty()
+	cve_dato = ndb.IntegerProperty()
+	saldo = ndb.FloatProperty()
+	
+	dl_institucion = ndb.StringProperty()
+	dl_dato = ndb.StringProperty()
+	dl_TEC = ndb.StringProperty()
