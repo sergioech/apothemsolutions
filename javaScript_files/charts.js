b1f195288a02ce3010ad931b4fe958f6c6bc573d
amigabledave <amigabledave@gmail.com>
2017-02-12 12:45:41 -0600

    Hace que el boton parezca estar seleccionado y ademas se muestro o oculte la lista de cortes.

--- javaScript_files/charts.js
+++ javaScript_files/charts.js
@@ -2,13 +2,6 @@
 // Load the Visualization API and the corechart package.
 google.charts.load('current', {'packages':['corechart']});
 
-// Set a callback to run when the Google Visualization API is loaded.
-//google.charts.setOnLoadCallback(drawChart);
-
-// Callback that creates and populates a data table,
-// instantiates the pie chart, passes in the data and
-// draws it.
-
 $(document).on('click', '.UpdateChartButton', function(){  
   var variable = $('#variable option:selected').val()
   var renglones = $('#renglones option:selected').val()
@@ -26,15 +19,11 @@ $(document).on('click', '.UpdateChartButton', function(){
     })
   })
   .done(function(raw_data){
-    // var chart_data = new google.visualization.DataTable();
-    // console.log(raw_data);
-    // var chart_array = [];
     var chart_array = raw_data['chart_array'];
     console.log(chart_array);
 
     var chart_data = google.visualization.arrayToDataTable(chart_array)
 
-    // Set chart options
     $('#chart_lead').text(raw_data['title']);
 
     var options = {
@@ -47,96 +36,7 @@ $(document).on('click', '.UpdateChartButton', function(){
       
     };
 
-
-    // Instantiate and draw our chart, passing in some options.
-    //var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
     var chart = new google.charts.Bar(document.getElementById('chart_div'));
     chart.draw(chart_data, options);
   })
 });
-
-
-
-// function requestChartData(){
-//   $.ajax({
-//     type: "POST",
-//     url: "/CNBVQueries",
-//     dataType: 'json',
-//     data: JSON.stringify({
-//       'data_requested': 'TestDataCNBV', 
-//     })
-//   })
-//   .done(function(data){
-//     return data['chartData']
-//     });    
-// };
-
-// function drawChart() {
-
-//   // Create the data table.
-  
-  
-//   $.ajax({
-//     type: "POST",
-//     url: "/CNBVQueries",
-//     dataType: 'json',
-//     data: JSON.stringify({
-//       'data_requested': 'TestDataCNBV', 
-//     })
-//   })
-//   .done(function(raw_data){
-//     var data = new google.visualization.DataTable();
-//     var raw_data = raw_data ['chartData'];
-//       console.log(raw_data)
-
-//     for (i in raw_data['columns']){
-//       column = raw_data['columns'][i]
-//       //console.log(column)
-//       data.addColumn(column[0],column[1]);
-//     };
-
-//     data.addRows(raw_data['rows'])
-//     // for ( i in raw_data['rows'] ){
-//     //    row = raw_data['rows'][i]
-//     //    console.log(raw_data['rows'])
-//     //    console.log(row)
-//     //    data.addRows(rows)
-//     // };
-
-//     // Set chart options
-//     var options = {'title':'Distribucion de numero de acreditados',
-//                    'width':400,
-//                    'height':300};
-
-//     // Instantiate and draw our chart, passing in some options.
-//     //var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-//     var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-//     chart.draw(data, options);
-//   }); 
-// }
-
-
-// La funcion original
-// function drawChart() {
-
-//   // Create the data table.
-//   var data = new google.visualization.DataTable();
-//   data.addColumn('string', 'Topping');
-//   data.addColumn('number', 'Slices');
-//   data.addRows([
-//     ['Mushrooms', 3],
-//     ['Onions', 1],
-//     ['Olives', 1],
-//     ['Zucchini', 1],
-//     ['Pepperoni', 2]
-//   ]);
-
-//   // Set chart options
-//   var options = {'title':'How Much Pizza I Ate Last Night',
-//                  'width':400,
-//                  'height':300};
-
-//   // Instantiate and draw our chart, passing in some options.
-//   var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-//   chart.draw(data, options);
-// }
